84c515956eeb1a24b98b47d08d5c1318d04db59c
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-20 03:10:55 -0500

    feat: drizzle orm setup

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, Config } from "drizzle-kit";
+
+const DATABASE_URL = process.env.DATABASE_URL!;
+
+// Create a new Drizzle instance
+
+const drizzleOptions = {
+  out: "./migrations",
+  schema: ["./src/db/schemas/products.ts"],
+  dialect: "postgresql",
+  dbCredentials: {
+    url: DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
+
+export default defineConfig(drizzleOptions);
