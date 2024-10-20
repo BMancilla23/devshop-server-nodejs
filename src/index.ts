4218c9772eb419446148dd3ddcd971dd943bfba5
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-20 03:14:58 -0500

    feat: routes added to server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 
+// Import the routes
+import productRoutes from "./routes/products/index";
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies (for form-data)
+app.use(express.urlencoded({ extended: false }));
+
+// Use the routes
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
